da67a90e67f4cd73c26bf44fe4d7a1b98fcd4217
waroad <78641034+waroad@users.noreply.github.com>
2021-07-18 23:57:45 +0900

    Create 15938_백준_골드4.py
    https://www.acmicpc.net/problem/15938

---
+++ DynamicProgramming/15938_백준_골드4.py
@@ -0,0 +1,10 @@
+import sys
+sys.setrecursionlimit(150000)
+x,y = [int(z) for z in input().split()]
+T=int(input())
+x_des,y_des=[int(z) for z in input().split()]
+num_of_obs=int(input())
+arr=[]
+for i in range (num_of_obs):
+    t1,t2=[int(x) for x in sys.stdin.readline().split()]
+    arr.append()
