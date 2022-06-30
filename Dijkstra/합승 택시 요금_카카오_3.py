87839daf47825bff1373abae5659b9f7b73b9018
AIN-Lab-DOHA <waroad@knu.ac.kr>
2022-06-30 15:36:37 +0900

    카카오 공채 코테 1차는 역시 쉬운 것 같다.

--- Dijkstra/합승 택시 요금_카카오_3.py
+++ Dijkstra/합승 택시 요금_카카오_3.py
@@ -38,7 +38,3 @@ def solution(n, s, a, b, fares):
     for i in range(n):
         ans=min(ans,dijkstra(i))
     return ans
-
-
-print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66],
-                            [2, 3, 22], [1, 6, 25]]))
